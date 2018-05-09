18d1feb4031cec4b26fa051dcd5c1d6ec7a07950
BashkoAnastasya <nastinm@mail.ru>
2018-05-09 23:20:24 +0300

    ТС

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,19 +3,20 @@ exports.config = {
     // The address of a running selenium server.
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
-      // Capabilities to be passed to the webdriver instance.
     capabilities: {
         browserName: 'chrome'
     },
-
-    // Spec patterns are relative to the configuration file location passed
-    // to protractor (in this example conf.js).
-    // They may include glob patterns.
-   // specs: ['spec//*.spec.js'],
+    onPrepare: function() {
+        var AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
+    },
     specs: ['spec//angulardoc.spec.js'],
 
-    // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
         showColors: true, // Use colors in the command line report.
     }
+
+
 };
